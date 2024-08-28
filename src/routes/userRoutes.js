de8fd4c15f0d7717c720291e9299def424c83860
varundasari <54391311+varundasari@users.noreply.github.com>
2024-08-28 15:52:58 +0530

    Delete src directory

--- src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-// Example route
-router.get('/profile', userController.getProfile);
-
-module.exports = router;
